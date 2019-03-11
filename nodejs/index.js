ba5d0afcd4f0d4912166a0934abf1a756e16d6e0
Tássio Jordão <tassio72@hotmail.com>
2019-03-10 23:26:28 -0300

    Separando o servidor por routes do node

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,32 @@
-const http = require ("http"); //o require incorpora algo ao código.Vamos incorporar a biblioteca http
-
-/*
-Vamos pegar o método createServer para criar o servidor.
-Vamos passar uma => como parametro, que vai ter outros dois parametros: request (req) e response(res)
-*/
+//const http = require ("http"); //o require incorpora algo ao código.Vamos incorporar a biblioteca http
 
 //importante: o comando crtl + c interrompe o servidor. Isso é importante porque qulquer alteração no code, para ser subida ao servidor, antes este deve ser interronpido.
+
+const express = require("express"); //o express já trás o http internamente
+
+let routesIndex = require("./routes/index");
+let routesUsers = require("./routes/users");
+
+let app = express()
+
+app.use(routesIndex);
+app.use("/users", routesUsers); //o parametro "/users" é a rota padrão desse requiremento
+
+/*
+Precisamos informar ao servidor qual a porta que ele ficará escutando os request
+Para aí executar as respondes
+como atribuimos a criação do servidor a variável server, vamos usa-la para qe fique escutando a porta 3000
+*/
+app.listen(3000, "127.0.0.1", () => { //no método listen, também passamos o número do IP que será criado.
+        console.log("servidor rodando");
+});
+
+
+
+/* COMO FICARIA SEM USAR O EXPRESS
+Vamos pegar o método createServer para criar o servidor.
+Vamos passar uma => como parametro, que vai ter outros dois parametros: request (req) e response(res)
+
 
 let server = http.createServer((req, res) => { 
 
@@ -50,11 +71,4 @@ let server = http.createServer((req, res) => {
 
 });
 
-/*
-Precisamos informar ao servidor qual a porta que ele ficará escutando os request
-Para aí executar as respondes
-como atribuimos a criação do servidor a variável server, vamos usa-la para qe fique escutando a porta 3000
 */
-server.listen(3000, "127.0.0.1", () => { //no método listen, também passamos o número do IP que será criado.
-        console.log("servidor rodando");
-})
---
+++ nodejs/routes/index.js
@@ -0,0 +1,29 @@
+let express = require ("express");
+let routes = express.Router();
+
+routes.get("/", (req, res) => { 
+
+    console.log(req.url);
+    console.log(req.method);
+
+        res.statusCode=200; //se o status da requisição foi bem sucedida
+        res.setHeader("content-type", "text/html"); //respondendo em formato HTML
+        res.end(
+            `<!DOCTYPE html>
+            <html lang="en">
+                <head>
+                <meta charset="UTF-8">
+                <title>Document</title>
+                </head>
+                <body>
+                    <h1>
+                        Olá
+                    </h1>
+                
+                </body>
+            </html>`
+        );
+
+});
+
+module.exports = routes;
